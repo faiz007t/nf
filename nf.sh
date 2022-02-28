317246342ec4584ee5bced921765f33954e5b36c
faiz007t <faiziz18@gmail.com>
2022-03-01 06:02:22 +0800

    Update nf.sh

--- nf.sh
+++ nf.sh
@@ -16,41 +16,41 @@ Font_Suffix="\033[0m";
 LOG_FILE="check.log";
 
 clear;
-echo -e "流媒体解锁测试" && echo -e "流媒体解锁测试" > ${LOG_FILE};
-echo -e "${Font_Purple}提示 本工具测试结果仅供参考，请以实际使用为准${Font_Suffix}" && echo -e "提示 本工具测试结果仅供参考，请以实际使用为准" >> ${LOG_FILE};
-echo -e "${Font_Yellow}流媒体合租平台 https://jcnf.xyz/nf${Font_Suffix}" && echo -e "流媒体合租平台 https://jcnf.xyz/nf" >> ${LOG_FILE};
-echo -e " ** 当前版本: v${shell_version}" && echo -e " ** 当前版本: v${shell_version}" >> ${LOG_FILE};
-echo -e " ** 系统时间: $(date)" && echo -e " ** 系统时间: $(date)" >> ${LOG_FILE};
+echo -e "Streaming Unlock Test" && echo -e "Streaming Unlock Test" > ${LOG_FILE};
+echo -e "${Font_Purple}Tips The test results of this tool are for reference only，Please refer to the actual use${Font_Suffix}" && echo -e "Tips The test results of this tool are for reference only，Please refer to the actual use" >> ${LOG_FILE};
+echo -e "${Font_Yellow}Streaming media sharing platform https://jcnf.xyz/nf${Font_Suffix}" && echo -e "Streaming media sharing platform https://jcnf.xyz/nf" >> ${LOG_FILE};
+echo -e " ** current version: v${shell_version}" && echo -e " ** current version: v${shell_version}" >> ${LOG_FILE};
+echo -e " ** system time: $(date)" && echo -e " ** system time: $(date)" >> ${LOG_FILE};
 
 export LANG="en_US";
 export LANGUAGE="en_US";
 export LC_ALL="en_US";
 
 function InstallJQ() {
-    #安装JQ
+    #Install JQ
     if [ -e "/etc/redhat-release" ];then
-        echo -e "${Font_Green}正在安装依赖: epel-release${Font_Suffix}";
+        echo -e "${Font_Green}installing dependencies: epel-release${Font_Suffix}";
         yum install epel-release -y -q > /dev/null;
-        echo -e "${Font_Green}正在安装依赖: jq${Font_Suffix}";
+        echo -e "${Font_Green}installing dependencies: jq${Font_Suffix}";
         yum install jq -y -q > /dev/null;
         elif [[ $(cat /etc/os-release | grep '^ID=') =~ ubuntu ]] || [[ $(cat /etc/os-release | grep '^ID=') =~ debian ]];then
-        echo -e "${Font_Green}正在更新软件包列表...${Font_Suffix}";
+        echo -e "${Font_Green}Updating package list...${Font_Suffix}";
         apt-get update -y > /dev/null;
-        echo -e "${Font_Green}正在安装依赖: jq${Font_Suffix}";
+        echo -e "${Font_Green}installing dependencies: jq${Font_Suffix}";
         apt-get install jq -y > /dev/null;
         elif [[ $(cat /etc/issue | grep '^ID=') =~ alpine ]];then
         apk update > /dev/null;
-        echo -e "${Font_Green}正在安装依赖: jq${Font_Suffix}";
+        echo -e "${Font_Green}installing dependencies: jq${Font_Suffix}";
         apk add jq > /dev/null;
     else
-        echo -e "${Font_Red}请手动安装jq${Font_Suffix}";
+        echo -e "${Font_Red}Please install jq manually${Font_Suffix}";
         exit;
     fi
 }
 
 function PharseJSON() {
-    # 使用方法: PharseJSON "要解析的原JSON文本" "要解析的键值"
-    # Example: PharseJSON ""Value":"123456"" "Value" [返回结果: 123456]
+    # Instructions: PharseJSON "raw JSON text to parse" "key value to parse"
+    # Example: PharseJSON ""Value":"123456"" "Value" [return result: 123456]
     echo -n $1 | jq -r .$2;
 }
 
@@ -64,12 +64,13 @@ function PasteBin_Upload() {
         --data "expiration=${PASTEBIN_EXPIRATION:-}" \
     --data "syntax=${PASTEBIN_SYNTAX:-text}")"
     if [ "$?" = "0" ]; then
-        echo -e "${Font_Green}已生成报告 ${uploadresult} ${Font_Suffix}";
+        echo -e "${Font_Green}Report generated ${uploadresult} ${Font_Suffix}";
     else
-        echo -e "${Font_Red}生成报告失败 ${Font_Suffix}";
+        echo -e "${Font_Red}Failed to generate report ${Font_Suffix}";
     fi
 }
 
+# Gaming Unlock Test - Steam
 function GameTest_Steam(){
     echo -n -e " Steam Currency:\t\t\t->\c";
     local result=`curl --user-agent "${UA_Browser}" -${1} -fsSL --max-time 30 https://store.steampowered.com/app/761830 2>&1 | grep priceCurrency | cut -d '"' -f4`;
@@ -81,12 +82,13 @@ function GameTest_Steam(){
     fi
 }
 
+# Streaming Unlock Test - HBONow
 function MediaUnlockTest_HBONow() {
     echo -n -e " HBO Now:\t\t\t\t->\c";
-    # 尝试获取成功的结果
+    # try to get a successful result
     local result=`curl --user-agent "${UA_Browser}" -${1} -fsSL --max-time 30 --write-out "%{url_effective}\n" --output /dev/null https://play.hbonow.com/ 2>&1`;
     if [[ "$result" != "curl"* ]]; then
-        # 下载页面成功，开始解析跳转
+        # The download page is successful, start parsing and jumping
         if [ "${result}" = "https://play.hbonow.com" ] || [ "${result}" = "https://play.hbonow.com/" ]; then
             echo -n -e "\r HBO Now:\t\t\t\t${Font_Green}Yes${Font_Suffix}\n" && echo " HBO Now:\t\t\t\tYes" >> ${LOG_FILE};
             elif [ "${result}" = "http://hbogeo.cust.footprint.net/hbonow/geo.html" ] || [ "${result}" = "http://geocust.hbonow.com/hbonow/geo.html" ]; then
@@ -95,12 +97,12 @@ function MediaUnlockTest_HBONow() {
             echo -n -e "\r HBO Now:\t\t\t\t${Font_Yellow}Failed (Parse Json)${Font_Suffix}\n" && echo -e " HBO Now:\t\t\t\tFailed (Parse Json)" >> ${LOG_FILE};
         fi
     else
-        # 下载页面失败，返回错误代码
+        # Download page failed, return error code
         echo -n -e "\r HBO Now:\t\t\t\t${Font_Red}Failed (Network Connection)${Font_Suffix}\n" && echo -e " HBO Now:\t\t\t\tFailed (Network Connection)" >> ${LOG_FILE};
     fi
 }
 
-# 流媒体解锁测试-动画疯
+# Streaming Unlock Test - Animation Crazy
 function MediaUnlockTest_BahamutAnime() {
     echo -n -e " Bahamut Anime:\t\t\t\t->\c";
     local tmpresult=`curl -${1} --user-agent "${UA_Browser}" --max-time 30 -fsSL 'https://ani.gamer.com.tw/ajax/token.php?adID=89422&sn=14667' 2>&1`;
@@ -133,11 +135,11 @@ function MediaUnlockTest_BahamutAnime() {
     fi
 }
 
-# 流媒体解锁测试-哔哩哔哩大陆限定
+# Streaming Unlock Test - Bilibili Mainland Limited
 function MediaUnlockTest_BilibiliChinaMainland() {
     echo -n -e " BiliBili China Mainland Only:\t\t->\c";
     local randsession="$(cat /dev/urandom | head -n 32 | md5sum | head -c 32)";
-    # 尝试获取成功的结果
+    # try to get a successful result
     local result=`curl --user-agent "${UA_Browser}" -${1} -fsSL --max-time 30 "https://api.bilibili.com/pgc/player/web/playurl?avid=82846771&qn=0&type=&otype=json&ep_id=307247&fourk=1&fnver=0&fnval=16&session=${randsession}&module=bangumi" 2>&1`;
     if [[ "$result" == "curl"* ]]; then
         echo -n -e "\r BiliBili China Mainland Only:\t\t${Font_Red}Failed (Network Connection)${Font_Suffix}\n" && echo -e " BiliBili China Mainland Only:\t\tFailed (Network Connection)" >> ${LOG_FILE};
@@ -163,11 +165,11 @@ function MediaUnlockTest_BilibiliChinaMainland() {
     esac
 }
 
-# 流媒体解锁测试-哔哩哔哩港澳台限定
+# Streaming Unlock Test - Bilibili Hong Kong, Macau and Taiwan Limited
 function MediaUnlockTest_BilibiliHKMCTW() {
     echo -n -e " BiliBili Hongkong/Macau/Taiwan:\t->\c";
     local randsession="$(cat /dev/urandom | head -n 32 | md5sum | head -c 32)";
-    # 尝试获取成功的结果
+    # try to get a successful result
     local result=`curl --user-agent "${UA_Browser}" -${1} -fsSL --max-time 30 "https://api.bilibili.com/pgc/player/web/playurl?avid=18281381&cid=29892777&qn=0&type=&otype=json&ep_id=183799&fourk=1&fnver=0&fnval=16&session=${randsession}&module=bangumi" 2>&1`;
     if [[ "$result" == "curl"* ]]; then
         echo -n -e "\r BiliBili Hongkong/Macau/Taiwan:\t${Font_Red}Failed (Network Connection)${Font_Suffix}\n" && echo -e " BiliBili Hongkong/Macau/Taiwan:\tFailed (Network Connection)" >> ${LOG_FILE};
@@ -192,11 +194,11 @@ function MediaUnlockTest_BilibiliHKMCTW() {
     esac
 }
 
-# 流媒体解锁测试-哔哩哔哩台湾限定
+# Streaming Unlock Test - Bilibili Taiwan Limited
 function MediaUnlockTest_BilibiliTW() {
     echo -n -e " Bilibili Taiwan Only:\t\t\t->\c";
     local randsession="$(cat /dev/urandom | head -n 32 | md5sum | head -c 32)";
-    # 尝试获取成功的结果
+    # try to get a successful result
     local result=`curl --user-agent "${UA_Browser}" -${1} -fsSL --max-time 30 "https://api.bilibili.com/pgc/player/web/playurl?avid=50762638&cid=100279344&qn=0&type=&otype=json&ep_id=268176&fourk=1&fnver=0&fnval=16&session=${randsession}&module=bangumi" 2>&1`;
     if [[ "$result" == "curl"* ]]; then
         echo -n -e "\r Bilibili Taiwan Only:\t\t\t${Font_Red}Failed (Network Connection)${Font_Suffix}\n" && echo -e " Bilibili Taiwan Only:Failed (Network Connection)" >> ${LOG_FILE};
@@ -222,8 +224,7 @@ function MediaUnlockTest_BilibiliTW() {
     esac
 }
 
-# 流媒体解锁测试-Abema.TV
-#
+# Streaming Unlock Test - Abema.TV
 function MediaUnlockTest_AbemaTV_IPTest() {
     echo -n -e " Abema.TV:\t\t\t\t->\c";
     #
@@ -247,6 +248,7 @@ function MediaUnlockTest_AbemaTV_IPTest() {
     echo -n -e "\r Abema.TV:\t\t\t\t${Font_Yellow}Oversea Only${Font_Suffix}\n" && echo -e " Abema.TV:\t\t\t\tOversea Only" >> ${LOG_FILE};
 }
 
+# Gaming Unlock Test - Princess Connect Re:Dive Japan
 function MediaUnlockTest_PCRJP() {
     echo -n -e " Princess Connect Re:Dive Japan:\t->\c";
     local result=`curl --user-agent "${UA_Dalvik}" -${1} -fsL --write-out %{http_code} --output /dev/null --max-time 30 https://api-priconne-redive.cygames.jp/ 2>&1`;
@@ -266,6 +268,7 @@ function MediaUnlockTest_PCRJP() {
     esac
 }
 
+# Gaming Unlock Test - Pretty Derby Japan
 function MediaUnlockTest_UMAJP() {
     echo -n -e " Pretty Derby Japan:\t\t\t->\c";
     local result=`curl --user-agent "${UA_Dalvik}" -${1} -fsL --write-out %{http_code} --output /dev/null --max-time 30 https://api-umamusume.cygames.jp/`;
@@ -285,6 +288,7 @@ function MediaUnlockTest_UMAJP() {
     esac
 }
 
+# Gaming Unlock Test - Kancolle Japan
 function MediaUnlockTest_Kancolle() {
     echo -n -e " Kancolle Japan:\t\t\t->\c";
     local result=`curl --user-agent "${UA_Dalvik}" -${1} -fsL --write-out %{http_code} --output /dev/null --max-time 30 http://203.104.209.7/kcscontents/ 2>&1`;
@@ -304,6 +308,7 @@ function MediaUnlockTest_Kancolle() {
     esac
 }
 
+# Streaming Unlock Test - BBC
 function MediaUnlockTest_BBC() {
     echo -n -e " BBC:\t\t\t\t\t->\c";
     local result=`curl --user-agent "${UA_Browser}" -${1} -fsL --write-out %{http_code} --output /dev/null --max-time 30 http://ve-dash-uk.live.cf.md.bbci.co.uk/`;
@@ -318,6 +323,7 @@ function MediaUnlockTest_BBC() {
     fi
 }
 
+# Streaming Unlock Test - Netflix
 function MediaUnlockTest_Netflix() {
     echo -n -e " Netflix:\t\t\t\t->\c";
     local result=`curl -${1} --user-agent "${UA_Browser}" -sSL "https://www.netflix.com/" 2>&1`;
@@ -358,6 +364,7 @@ function MediaUnlockTest_Netflix() {
     return;
 }
 
+# Streaming Unlock Test - YouTube
 function MediaUnlockTest_YouTube_Region() {
     echo -n -e " YouTube Region:\t\t\t->\c";
     local result=`curl --user-agent "${UA_Browser}" -${1} -sSL "https://www.youtube.com/" 2>&1`;
@@ -377,6 +384,7 @@ function MediaUnlockTest_YouTube_Region() {
     return;
 }
 
+# Streaming Unlock Test - DisneyPlus
 function MediaUnlockTest_DisneyPlus() {
     echo -n -e " DisneyPlus:\t\t\t\t->\c";
     local result=`curl -${1} --user-agent "${UA_Browser}" -sSL "https://global.edge.bamgrid.com/token" 2>&1`;
@@ -417,6 +425,7 @@ function MediaUnlockTest_DisneyPlus() {
     echo -n -e "\r DisneyPlus:\t\t\t\t${Font_Red}No${Font_Suffix}\n" && echo -e " DisneyPlus:\t\t\t\tNo" >> ${LOG_FILE};
 }
 
+# Streaming Unlock Test - DAZN
 function MediaUnlockTest_Dazn() {
     echo -n -e " Dazn:\t\t\t\t\t->\c";
     local result=`curl -${1} -sSL --max-time 30 -X POST -H "Content-Type: application/json" -d '{"LandingPageKey":"generic","Languages":"zh-CN,zh,en","Platform":"web","PlatformAttributes":{},"Manufacturer":"","PromoCode":"","Version":"2"}' "https://startup.core.indazn.com/misl/v5/Startup" 2>&1`;
@@ -438,6 +447,7 @@ function MediaUnlockTest_Dazn() {
     echo -n -e "\r Dazn:\t\t\t\t\t${Font_Green}Yes(Region: ${region})${Font_Suffix}\n" && echo -e " Dazn:\t\t\t\t\tYes(Region: ${region})" >> ${LOG_FILE}
 }
 
+# Streaming Unlock Test - Hulu Japan
 function MediaUnlockTest_HuluJP() {
     echo -n -e " Hulu Japan:\t\t\t\t->\c";
     local result=`curl -${1} -sSL -o /dev/null --max-time 30 -w '%{url_effective}\n' "https://id.hulu.jp" 2>&1`;
@@ -453,6 +463,7 @@ function MediaUnlockTest_HuluJP() {
     echo -n -e "\r Hulu Japan:\t\t\t\t${Font_Red}No${Font_Suffix}\n" && echo -e " Hulu Japan:\t\t\t\tNo" >> ${LOG_FILE};
 }
 
+# Streaming Unlock Test - MyTVSuper
 function MediaUnlockTest_MyTVSuper() {
     echo -n -e " MyTVSuper:\t\t\t\t->\c";
     local result=`curl -sSL -${1} --max-time 30 "https://www.mytvsuper.com/iptest.php" 2>&1`;
@@ -469,6 +480,7 @@ function MediaUnlockTest_MyTVSuper() {
     echo -n -e "\r MyTVSuper:\t\t\t\t${Font_Red}No${Font_Suffix}\n" && echo -e " MyTVSuper:\t\t\t\tNo" >> ${LOG_FILE};
 }
 
+# Streaming Unlock Test - Now E
 function MediaUnlockTest_NowE() {
     echo -n -e " Now E:\t\t\t\t\t->\c";
     local result=`curl -${1} -sSLk --max-time 30 -X POST -H "Content-Type: application/json" -d '{"contentId":"202105121370235","contentType":"Vod","pin":"","deviceId":"W-60b8d30a-9294-d251-617b-c12f9d0c","deviceType":"WEB"}' "https://webtvapi.nowe.com/16/1/getVodURL" 2>&1`;
@@ -491,6 +503,7 @@ function MediaUnlockTest_NowE() {
     esac
 }
 
+# Streaming Unlock Test - Viu TV
 function MediaUnlockTest_ViuTV() {
     echo -n -e " Viu TV:\t\t\t\t->\c";
     local result=`curl -${1} -sSLk --max-time 30 -X POST -H "Content-Type: application/json" -d '{"callerReferenceNo":"20210603233037","productId":"202009041154906","contentId":"202009041154906","contentType":"Vod","mode":"prod","PIN":"password","cookie":"3c2c4eafe3b0d644b8","deviceId":"U5f1bf2bd8ff2ee000","deviceType":"ANDROID_WEB","format":"HLS"}' "https://api.viu.now.com/p8/3/getVodURL" 2>&1`;
@@ -513,6 +526,7 @@ function MediaUnlockTest_ViuTV() {
     echo -n -e "\r Viu.TV:\t\t\t\t${Font_Red}Failed (Unexpected Result: $result)${Font_Suffix}\n" && echo -e " Viu TV:\t\t\t\tFailed (Unexpected Result: $result)" >> ${LOG_FILE};
 }
 
+# Streaming Unlock Test - U Next
 function MediaUnlockTest_UNext() {
     echo -n -e " U Next:\t\t\t\t->\c";
     local result=`curl -${1} -sSL --max-time 30 "https://video-api.unext.jp/api/1/player?entity%5B%5D=playlist_url&episode_code=ED00148814&title_code=SID0028118&keyonly_flg=0&play_mode=caption&bitrate_low=1500" 2>&1`;
@@ -534,6 +548,7 @@ function MediaUnlockTest_UNext() {
     echo -n -e "\r U Next:\t\t\t\t${Font_Red}Failed (Unexpected Result: ${result})${Font_Suffix}\n" && echo -e " U Next:\t\t\t\tFailed (Unexpected Result: ${result})" >> ${LOG_FILE};
 }
 
+# Streaming Unlock Test - Paravi
 function MediaUnlockTest_Paravi() {
     echo -n -e " Paravi:\t\t\t\t->\c";
     local result=`curl -${1} -sSL --max-time 30 -H "Content-Type: application/json" -d '{"meta_id":71885,"vuid":"3b64a775a4e38d90cc43ea4c7214702b","device_code":1,"app_id":1}' "https://api.paravi.jp/api/v1/playback/auth" 2>&1`;
@@ -555,6 +570,7 @@ function MediaUnlockTest_Paravi() {
     echo -n -e "\r Paravi:\t\t\t\t${Font_Red}No${Font_Suffix}\n" && echo -e " Paravi:\t\t\t\tNo" >> ${LOG_FILE};
 }
 
+# Checking ISP
 function ISP(){
     local result=`curl -sSL -${1} "https://api.ip.sb/geoip" 2>&1`;
     if [[ "$result" == "curl"* ]];then
@@ -568,27 +584,25 @@ function ISP(){
     fi
 }
 
+# Media Unlock Test Sites
 function MediaUnlockTest() {
     ISP ${1};
     MediaUnlockTest_HBONow ${1};
-    MediaUnlockTest_BBC ${1};
-    
+    MediaUnlockTest_BBC ${1};    
     MediaUnlockTest_MyTVSuper ${1};
     MediaUnlockTest_NowE ${1};
     MediaUnlockTest_ViuTV ${1};
     MediaUnlockTest_BahamutAnime ${1};
     MediaUnlockTest_BilibiliChinaMainland ${1};
     MediaUnlockTest_BilibiliHKMCTW ${1};
-    MediaUnlockTest_BilibiliTW ${1};
-    
+    MediaUnlockTest_BilibiliTW ${1};    
     MediaUnlockTest_AbemaTV_IPTest ${1};
     MediaUnlockTest_Paravi ${1};
     MediaUnlockTest_UNext ${1};
     MediaUnlockTest_HuluJP ${1};
     MediaUnlockTest_PCRJP ${1};
     MediaUnlockTest_UMAJP ${1};
-    MediaUnlockTest_Kancolle ${1};
-    
+    MediaUnlockTest_Kancolle ${1};    
     MediaUnlockTest_Dazn ${1};
     MediaUnlockTest_Netflix ${1};
     MediaUnlockTest_YouTube_Region ${1};
@@ -606,21 +620,21 @@ jq -V > /dev/null 2>&1;
 if [ $? -ne 0 ];then
     InstallJQ;
 fi
-echo " ** 正在测试IPv4解锁情况" && echo " ** 正在测试IPv4解锁情况" >> ${LOG_FILE};
+echo " ** Testing IPv4 Unlocking" && echo " ** Testing IPv4 Unlocking" >> ${LOG_FILE};
 check4=`ping 1.1.1.1 -c 1 2>&1`;
 if [[ "$check4" != *"unreachable"* ]] && [[ "$check4" != *"Unreachable"* ]];then
     MediaUnlockTest 4;
 else
-    echo -e "${Font_SkyBlue}当前主机不支持IPv4,跳过...${Font_Suffix}" && echo "当前主机不支持IPv4,跳过..." >> ${LOG_FILE};
+    echo -e "${Font_SkyBlue}The current host does not support IPv4, skip...${Font_Suffix}" && echo "The current host does not support IPv4, skip..." >> ${LOG_FILE};
 fi
 
-echo " ** 正在测试IPv6解锁情况" && echo " ** 正在测试IPv6解锁情况" >> ${LOG_FILE};
+echo " ** Testing IPv6 Unlocking" && echo " ** Testing IPv6 Unlocking" >> ${LOG_FILE};
 check6=`ping6 240c::6666 -c 1 2>&1`;
 if [[ "$check6" != *"unreachable"* ]] && [[ "$check6" != *"Unreachable"* ]];then
     MediaUnlockTest 6;
 else
-    echo -e "${Font_SkyBlue}当前主机不支持IPv6,跳过...${Font_Suffix}" && echo "当前主机不支持IPv6,跳过..." >> ${LOG_FILE};
+    echo -e "${Font_SkyBlue}The current host does not support IPv6, skip...${Font_Suffix}" && echo "The current host does not support IPv6, skip..." >> ${LOG_FILE};
 fi
 echo -e "";
-echo -e "${Font_Green}本次测试结果已保存到 ${LOG_FILE} ${Font_Suffix}";
+echo -e "${Font_Green}The test results have been saved to ${LOG_FILE} ${Font_Suffix}";
 cat ${LOG_FILE} | PasteBin_Upload;
