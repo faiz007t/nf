7f962b4ae3791d33dfba54d0f11ef531174ecdf7
faiz007t <faiziz18@gmail.com>
2022-04-26 02:02:28 +0800

    Update nf.sh

--- nf.sh
+++ nf.sh
@@ -19,7 +19,7 @@ clear;
 echo -e "${Font_Yellow} **Testing IPv4 unlocking** ${Font_Suffix}" && echo -e " **Testing IPv4 unlocking** " > ${LOG_FILE};
 echo -e "${Font_Yellow} -----------------------------------------------\n" && echo -e " -----------------------------------------------\n" >> ${LOG_FILE};
 echo -e " **Your network is:" && echo -e " **Your network is:" >> ${LOG_FILE};
-echo -e "\n==============[ Checking Sites ]===============" && echo -e "\n==============[ Checking Sites ]===============\n" >> ${LOG_FILE};
+echo -e "\n==============[ Checking Sites ]===============${Font_Suffix}" && echo -e "\n==============[ Checking Sites ]===============\n" >> ${LOG_FILE};
 
 export LANG="en_US";
 export LANGUAGE="en_US";
