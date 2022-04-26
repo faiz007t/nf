ee0a10e6f149cdfb23aa5b416e94bd5c5e40d65e
faiz007t <faiziz18@gmail.com>
2022-04-26 10:52:59 +0800

    Update nf.sh

--- nf.sh
+++ nf.sh
@@ -134,7 +134,7 @@ function MediaUnlockTest_Netflix() {
     if [[ ! -n "$region" ]];then
         region="US";
     fi
-    echo -n -e "\r Netflix:\t\t\t\t${Font_Green} ${region})${Font_Suffix}\n" && echo -e " Netflix:\t\t\t\t ${region})" >> ${LOG_FILE};
+    echo -n -e "\r Netflix:\t\t\t\t${Font_Green} ${region})${Font_Suffix}\n" && echo -e " Netflix:\t\t\t\t${region}" >> ${LOG_FILE};
     return;
 }
 
@@ -201,7 +201,7 @@ function MediaUnlockTest_DisneyPlus() {
 		echo -n -e "\r Disney+:\t\t\t\t${Font_Red}No${Font_Suffix}\n"
 		return
 	elif [ -n "$region" ] && [[ "$inSupportedLocation" == "true" ]]; then
-		echo -n -e "\r Disney+:\t\t\t\t${Font_Green}${Font_Suffix}\n"
+		echo -n -e "\r Disney+:\t\t\t\t${Font_Green}$region${Font_Suffix}\n"
 		return
 	elif [ -z "$region" ]; then
 		echo -n -e "\r Disney+:\t\t\t\t${Font_Red}No${Font_Suffix}\n"
@@ -232,7 +232,7 @@ function MediaUnlockTest_Dazn() {
         echo -n -e "\r Dazn:\t\t\t\t\t${Font_Red}No${Font_Suffix}\n" && echo -e " Dazn:\t\t\t\t\tNo" >> ${LOG_FILE}
         return;
     fi
-    echo -n -e "\r Dazn:\t\t\t\t\t${Font_Green}${region})${Font_Suffix}\n" && echo -e " Dazn:\t\t\t\t\t${region})" >> ${LOG_FILE}
+    echo -n -e "\r Dazn:\t\t\t\t\t${Font_Green}${region})${Font_Suffix}\n" && echo -e " Dazn:\t\t\t\t\t${region}" >> ${LOG_FILE}
 }
 
 # Streaming Unlock Test - HuluJP
