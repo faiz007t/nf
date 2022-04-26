cee974153a6ded6e71d3f30cbaedd73909d9d950
faiz007t <faiziz18@gmail.com>
2022-04-26 11:32:18 +0800

    Update nf.sh

--- nf.sh
+++ nf.sh
@@ -138,8 +138,8 @@ function MediaUnlockTest_Netflix() {
     return;
 }
 
-# Streaming Unlock Test - Youtube Region
-function MediaUnlockTest_YouTubeRegion() {
+# Streaming Unlock Test - Youtube
+function MediaUnlockTest_YouTube() {
     echo -n -e " YouTube:\t\t\t\t->\c";
     local result=`curl --user-agent "${UA_Browser}" -${1} -sSL "https://www.youtube.com/" 2>&1`;
     
@@ -286,11 +286,11 @@ function MediaUnlockTest() {
     MediaUnlockTest_Dazn ${1};
     MediaUnlockTest_DisneyPlus ${1};
     MediaUnlockTest_HBONow ${1};
-    MediaUnlockTest_HuluJP ${1};
+    MediaUnlockTest_Hulu ${1};
     MediaUnlockTest_Netflix ${1};
     MediaUnlockTest_Steam ${1};
     MediaUnlockTest_ViuTV ${1};
-    MediaUnlockTest_YouTubeRegion ${1};
+    MediaUnlockTest_YouTube ${1};
 }
 
 curl -V > /dev/null 2>&1;
